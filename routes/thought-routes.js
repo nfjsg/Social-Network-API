68f347a9dc8862ca97d5396a1bdb1c8639663b97
nfjsg <dasarathan@icloud.com>
2023-12-10 17:34:24 -0500

    Update thought-routes.js

--- routes/thought-routes.js
+++ routes/thought-routes.js
@@ -1,1 +1,24 @@
+const { Router } = require('express');
+const {
+  getAllThoughts,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction,
+} = require('../controllers/thought-controller');
+
+const router = Router();
+
+// Define routes
+router.get('/thoughts', getAllThoughts);
+router.get('/thoughts/:id', getThoughtById);
+router.post('/thoughts', createThought);
+router.put('/thoughts/:id', updateThought);
+router.delete('/thoughts/:id', deleteThought);
+router.post('/thoughts/:thoughtId/reactions', createReaction);
+router.delete('/thoughts/:thoughtId/reactions/:reactionId', deleteReaction);
+
+module.exports = router;
 
