409ced11f5eb8a354c7c98b34f8663da3078254a
nfjsg <dasarathan@icloud.com>
2023-12-10 17:34:09 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,12 @@
 
+const { Router } = require('express');
+const userRoutes = require('./user-routes');
+const thoughtRoutes = require('./thought-routes');
+
+const router = Router();
+
+// Use user and thought routes
+router.use('/api', userRoutes);
+router.use('/api', thoughtRoutes);
+
+module.exports = router;
